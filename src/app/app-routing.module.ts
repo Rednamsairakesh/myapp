bcdb53231d822a196034008154767cbedd3395b7
Rednamsairakesh <sairakeshrednam@gmail.com>
2022-12-08 09:48:36 +0530

    basic routing implemented with login and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'calculator', component: CalculatorComponent},
+  {path:'', component: LoginComponent},
+  {path:'**', component: PagenotfoundComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
