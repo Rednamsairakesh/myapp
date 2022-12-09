67239e477d0774753428656abc960a056a783332
Rednamsairakesh <sairakeshrednam@gmail.com>
2022-12-09 11:57:27 +0530

    attribute directives

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -7,14 +7,16 @@ import { Component } from '@angular/core';
 })
 export class DirectivesComponent {
 public names:string[]= ['srk','rakesh','guna']
-public states:string[]= ['AN','AP','AR','AS','BR','CG','CH','DD','DL','GA',
-'GJ','HP','JH','JK','KA','KL']
+public states:string[]= ['AN','AP','AR','AS','BR','CG'
+,'CH','DD','DL','GA','GJ','HP','JH','JK','KA','KL']
 public users:any =[
-  {name:'Anil1',age:20,package:10},
-  {name:'Anil2',age:21,package:11},
-  {name:'Anil3',age:22,package:11},
-  {name:'Anil4',age:21,package:12},
+  {name:'Anil1',age:18,package:10},
+  {name:'Anil2',age:20,package:9},
+  {name:'Anil3',age:22,package:8},
+  {name:'Anil4',age:31,package:12},
   {name:'Anil5',age:23,package:11}
 ]
 public time:number=22
+
+public today:any = new Date();
 }
