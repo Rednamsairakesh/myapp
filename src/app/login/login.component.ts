56eafad05c044fa4fbc2de5bb43b2f8ff1b28289
Rednamsairakesh <sairakeshrednam@gmail.com>
2022-12-19 15:09:34 +0530

    updated login authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { VirtualTimeScheduler } from 'rxjs';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+ 
+  public loginForm:FormGroup = new FormGroup(
+    {
+    email: new FormControl(),
+    password: new FormControl()
+  }
+  )
+  constructor(private _loginService:LoginService, private _router:Router) {}
+
+  submit() {
+    console.log(this.loginForm);
+    this._loginService.login(this.loginForm.value).subscribe(
+      (data:any)=>{
+        alert("login success");
+        this._router.navigateByUrl("/dashboard");
+      },
+      (err:any)=>{
+        alert("Invalid credentials");
+      }
+    )
+  }
 
 }
